80a06149cb2b0406723740a31ed5ae10c98eee97
HasanAlii73 <ha1338967@gmail.com>
2026-08-11 12:31:59 +0300

    get user fuction

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,3 +1,9 @@
 export const getToken = () => localStorage.getItem("token");
 export const setToken = (token) => localStorage.setItem("token", token);
 export const removeToken = () => localStorage.removeItem("token");
+export const getUser = () => {
+    const token = getToken();
+    if(!token) return null;
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload;
+};
